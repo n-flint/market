9a34ad281207eb766a138683873bf8d87e84df80
Noah Flint <noah.flint24@gmail.com>
2019-01-08 10:29:59 -0700

    finished iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -29,6 +29,33 @@ class Market
     vendors
   end
 
+  def sorted_item_list
+    list = []
+    @vendors.each do |vendor|
+      list << vendor.inventory.keys
+    end
+    list.flatten.uniq.sort
+  end
+
+  #tried to reuse my sorted item list, but i dont think i want my keys in my inventory hash to be in alphabetical order, according to the interaction pattern
+  # def total_inventory
+  #   inventory = Hash.new
+  #   sorted_item_list.each do |item|
+  #     inventory[item] = 0
+  #   end
+  #   inventory
+  # end
+
+  def total_inventory
+    inventory = Hash.new(0)
+    @vendors.each do |vendor|
+      vendor.inventory.keys.each do |item|
+        inventory[item] += vendor.inventory[item]
+      end
+    end
+    inventory
+  end
+
 
 
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -81,24 +81,41 @@ class MarketTest < MiniTest::Test
     assert_equal [vendor_2], market.vendors_that_sell("Banana Nice Cream")
   end
 
-  # def test_it_has_a_list_of_all_items_sorted_alphabetically
-  #   market = Market.new("South Pearl Street Farmers Market")
-  #   vendor_1 = Vendor.new("Rocky Mountain Fresh")
-  #   vendor_1.stock("Peaches", 35)
-  #   vendor_1.stock("Tomatoes", 7)
-  #   vendor_2 = Vendor.new("Ba-Nom-a-Nom")
-  #   vendor_2.stock("Banana Nice Cream", 50)
-  #   vendor_2.stock("Peach-Raspberry Nice Cream", 25)
-  #   vendor_3 = Vendor.new("Palisade Peach Shack")
-  #   vendor_3.stock("Peaches", 65)
-  #   market.add_vendor(vendor_1)
-  #   market.add_vendor(vendor_2)
-  #   market.add_vendor(vendor_3)
-  #
-  #   expected = ["Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peaches", "Tomatoes"]
-  #
-  #
-  # end
+  def test_it_has_a_list_of_all_items_sorted_alphabetically
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+
+    expected = ["Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peaches", "Tomatoes"]
+    assert_equal expected, market.sorted_item_list
+  end
+
+  def test_it_has_a_hash_of_total_inventory
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+
+    expected = {"Peaches"=>100, "Tomatoes"=>7, "Banana Nice Cream"=>50, "Peach-Raspberry Nice Cream"=>25}
+    assert_equal expected, market.total_inventory
+  end
 
 
 
